da6c73dbc31d2f18b840df877bbaf378d3f1ebbb
Oswaldo <salasoswaldoa@gmail.com>
2021-11-16 17:15:27 -0400

    algunas mejoras

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -3,69 +3,48 @@ import { Context } from '../../store/appContext'
 import { useParams} from 'react-router-dom';
 import Carousel from 'react-elastic-carousel';
 import { Cards } from '../Home/components';
+import './styles.css'
 
 const Profile = () => {
     const {store, actions}=useContext(Context);
     const {id}= useParams()
-    console.log(id)
-    
-    
-   
-    
- const ONG= store.dataONG.filter((ong)=>ong.id.toString()=== id)
-
-      console.log(ONG)
+     
+    const ONG= store.dataONG.filter((ong)=>ong.id.toString()=== id)
+     
+      console.log(ONG[0])
     const breakPoints = [
         {width:100, itemsToShow:3},
     ]
 
    
     return (
-        <>
-       
-            
+        <div className='container '>
+            <div className=' container-name'>
+
+                <h2 className='name'>{ONG[0].ong_name}</h2>
+            </div>
             <div>
-                    <div>
-                        ONG={ONG[0].ong_name} 
-                    </div>
-                    <div>
-                        location = {ONG[0].location}
-                    </div>
-                    <div>
 
-                image= {ONG[0].image}
-                    </div>
-                    <div>
-
-                logo = {ONG[0].logo}
-                    </div>
-
-                    <div>
-
-                website_address ={ONG[0].website_addrres}
-                    </div>
-                rif= {ONG[0].rif}
-                bank= {ONG[0].bank}
-                account= {ONG[0].account}
-                description= {ONG[0].description}
-                
-
-
-        </div>
+            <img src={ONG[0].image} className='image'/>
+            </div>
+               <p>{ONG[0].description}</p>
+        
+       
+         
         
         <Carousel className = 'CarruselEvent' breakPoints={breakPoints}>
-        {ONG[0].activities.map((activity)=>(
-            <Cards key={activity.id} id={activity.id}/>
+            {ONG[0].activities.map((activity)=>(
+                <Cards key={activity.id} id={activity.id}/>
 
-        ))}
+            ))}
       
-    </Carousel>
+       </Carousel>
     
         
         
        
         
-        </>
+        </div>
     )
 
 }
